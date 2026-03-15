78e20283748dbc4d327464a3313ee2f0ceb64910
Advait Bagri <advait.bagri@gmail.com>
2026-03-15 17:35:01 +0800

    AddCardArgs: Add JavaDoc comments for AddCardArgs and each of its methods

--- src/main/java/seedu/flashcli/parser/AddCardArgs.java
+++ src/main/java/seedu/flashcli/parser/AddCardArgs.java
@@ -1,11 +1,17 @@
 package seedu.flashcli.parser;
 
+/**
+ * Immutable object grouping the three arguments needed for the addCard comment.
+ */
 public class AddCardArgs {
 
     private final String deckName;
     private final String question;
     private final String answer;
 
+    /**
+     * Constructs an AddCardArgs with all three addCard fields.
+     */
     public AddCardArgs(String deckName, String question, String answer) {
         this.deckName = deckName;
         this.question = question;
