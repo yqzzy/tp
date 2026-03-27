69497047f05f6903536983467a04b53e00304427
infinitymehs <106293915+infinitymehs@users.noreply.github.com>
2026-03-28 05:28:19 +0800

    Update EditCardCommand.java

--- src/main/java/seedu/flashcli/command/EditCardCommand.java
+++ src/main/java/seedu/flashcli/command/EditCardCommand.java
@@ -15,18 +15,18 @@ public class EditCardCommand implements Command{
     private final String question;
     private final String answer;
 
-  public EditCardCommand(EditCardArgs args){
-    this.deckName = args.getDeckName();
-    this.cardIndex = args.getCardIndex();
-    this.question = args.getQuestion();
-    this.answer = args.getAnswer();
-  }
+    public EditCardCommand(EditCardArgs args){
+      this.deckName = args.getDeckName();
+      this.cardIndex = args.getCardIndex();
+      this.question = args.getQuestion();
+      this.answer = args.getAnswer();
+    }
 
-  @Override
-  public boolean execute(DeckManager deckManager, Ui ui, Scanner in) throws FlashException{
-    Deck deck = deckManager.getDeck(deckName);
-    Card card = deck.editCard(cardIndex, question, answer);
-    ui.showCardEdited(card, deckName);
-    return false;
-  }
+    @Override
+    public boolean execute(DeckManager deckManager, Ui ui, Scanner in) throws FlashException{
+      Deck deck = deckManager.getDeck(deckName);
+      Card card = deck.editCard(cardIndex, question, answer);
+      ui.showCardEdited(card, deckName);
+      return false;
+    }
 }
