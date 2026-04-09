79538b3e458460c85771d3806e8ae19ec31c9447
Sid3024 <siddharth.arum@gmail.com>
2026-04-10 03:55:50 +0800

    Add command format enum
    Each enum is associated with a message showing the appropriate format for each command.

---
+++ src/main/java/seedu/flashcli/exception/CommandFormat.java
@@ -0,0 +1,26 @@
+package seedu.flashcli.exception;
+
+public enum CommandFormat {
+
+    ADD_CARD("Use: addCard d/<deck> q/<question> a/<answer>"),
+    DELETE_CARD("Use: deleteCard d/<deck> i/<index>"),
+    EDIT_CARD("Use: editCard d/<deck> i/<index> q/<new question> a/<new answer>"),
+    CREATE_DECK("Use: createDeck d/<deck>"),
+    DELETE_DECK("Use: deleteDeck d/<deck>"),
+    CLEAR_DECK("Use: clearDeck d/<deck>"),
+    LIST_CARDS("Use: listCards d/<deck>"),
+    LIST_DECKS("Use: listDecks"),
+    STUDY("Use: study d/<deck>"),
+    HELP("Use: help"),
+    EXIT("Use: exit");
+
+    private final String format;
+
+    CommandFormat(String format) {
+        this.format = format;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+}
